ee933bf16c87da779d15b377c58d788240ada005
Sahib111 <58338778+Sahib111@users.noreply.github.com>
2021-08-28 14:56:19 +0530

    Update script.js

--- Preloader-1/script.js
+++ Preloader-1/script.js
@@ -1,7 +1,7 @@
-import url("https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js");
+import url('https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js');
 $(document).ready(function() {
 //Preloader
-preloaderFadeOutTime = 6000;
+preloaderFadeOutTime = 1000;
 function hidePreloader() {
 var preloader = $(&#39;.spinner-wrapper&#39;);
 preloader.fadeOut(preloaderFadeOutTime);
