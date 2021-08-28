269e9e4d0789625b47d46d40416d18f128235f7e
Sahib111 <58338778+Sahib111@users.noreply.github.com>
2021-08-28 16:26:05 +0530

    Delete Preloader-1 directory

--- Preloader-1/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-//Preloader
-preloaderFadeOutTime = 2000;
-function hidePreloader() {
-var preloader = $(&#39;.spinner-wrapper&#39;);
-preloader.fadeOut(preloaderFadeOutTime);
-}
-hidePreloader();
-});
