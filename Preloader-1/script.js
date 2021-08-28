92aacdcc60ebf33251f6b21d42bebc3ab1964d12
Sahib111 <58338778+Sahib111@users.noreply.github.com>
2021-08-28 16:09:09 +0530

    Update script.js

--- Preloader-1/script.js
+++ Preloader-1/script.js
@@ -1,7 +1,6 @@
-
 $(document).ready(function() {
 //Preloader
-preloaderFadeOutTime = 1000;
+preloaderFadeOutTime = 2000;
 function hidePreloader() {
 var preloader = $(&#39;.spinner-wrapper&#39;);
 preloader.fadeOut(preloaderFadeOutTime);
