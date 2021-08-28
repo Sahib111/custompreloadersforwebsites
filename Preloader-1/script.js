43c942606602eccf5a0d83c1b3777ee7a5317788
Sahib111 <58338778+Sahib111@users.noreply.github.com>
2021-08-28 14:53:22 +0530

    Update script.js

--- Preloader-1/script.js
+++ Preloader-1/script.js
@@ -1,4 +1,4 @@
-import url("https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js");
+import url("https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js");
 $(document).ready(function() {
 //Preloader
 preloaderFadeOutTime = 6000;
