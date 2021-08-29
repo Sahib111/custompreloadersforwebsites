15a794b43009ffbbefd5dde0feaef8c70df6168d
Sahib111 <58338778+Sahib111@users.noreply.github.com>
2021-08-29 09:01:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-document.body.classList.add('js-loading'); 
-window.addEventListener("load", showPage); 
-function showPage() {document.body.classList.remove('js-loading'); 
-}
-
 $(window).load(function() { 
 $(".preloader").fadeOut("slow");
-};
+});
